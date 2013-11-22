f6f99d2749443b91ba01edcc74e2ae58c62686c2
Karol <c.cisowski+code@gmail.com>
2013-11-22 19:20:08 +0100

    photo in entry activity

--- gen/com/my/places/R.java
+++ gen/com/my/places/R.java
@@ -22,10 +22,12 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int start=0x7f020001;
+        public static final int ship=0x7f020001;
+        public static final int start=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int imageView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_entry=0x7f030000;
