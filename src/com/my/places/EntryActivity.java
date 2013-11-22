5fb697a6b7fb97d9f037a15c393444b2249a919c
Karol <c.cisowski+code@gmail.com>
2013-11-22 18:17:05 +0100

    hidding the UI Bar

--- src/com/my/places/EntryActivity.java
+++ src/com/my/places/EntryActivity.java
@@ -1,14 +1,34 @@
 package com.my.places;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.WindowManager;
 
 public class EntryActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+			if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+		else
+		{
+			View decorView = getWindow().getDecorView();
+			// Hide the status bar.
+			int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+			decorView.setSystemUiVisibility(uiOptions);
+			// Remember that you should never show the action bar if the
+			// status bar is hidden, so hide that too if necessary.
+			ActionBar actionBar = getActionBar();
+			actionBar.hide();
+		}
 		setContentView(R.layout.activity_entry);
 	}
 
